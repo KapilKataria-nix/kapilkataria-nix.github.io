8ad85233b65dcd5cca0583aa806116e572071f7e
KapilKataria-nix <kapilkataria245@gmail.com>
2023-12-14 19:17:22 +0530

    Update js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 /*========================typed.js ======================*/
 
 const typed = new Typed('.multiple-text',{
-    strings: ['Coder','Ethical Hacker', 'Cloud Engineer', 'Devsecops Engineer'],
+    strings: ['Coder','Ethical Hacker', 'Cloud Engineer', 'DevOps Engineer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
